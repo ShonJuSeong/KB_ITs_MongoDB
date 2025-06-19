145c6a1b9a09fcd139c602e58049a7fc3cd2ee1e
손주성 <sow7722@gmail.com>
2025-06-19 16:17:39 +0900

    Add files via upload

---
+++ Spring/08_Spring/src/test/java/org/scoula/sample/service/SampleServiceImpl.java
@@ -0,0 +1,25 @@
+package org.scoula.sample.service;
+
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.scoula.config.RootConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = { RootConfig.class })
+@Log4j2
+
+class SampleServiceImpl {
+
+    @Autowired
+    private SampleService service;
+
+    @Test
+    void doAdd() throws Exception{
+        log.info(service.doAdd("100", "200"));
+    }
+}
